c5775d7a3f88ad934ad2fab14984a0df5f0ab295
hannjimnz <hannjimnz@gmail.com>
2026-05-28 07:21:36 -0600

    agregue instruscciones de ejecucuion en el main

--- main.py
+++ main.py
@@ -10,3 +10,6 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 def inicio():
     return {"mensaje": "Sistema funcionando"}
+
+#uvicorn main:app --reload   tiene que estar dentro del backend 
+#http://127.0.0.1:8000/docs
