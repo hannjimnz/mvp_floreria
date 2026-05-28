3dad3eed9157a2a6f947ba359effe0d6b3b50d22
hannjimnz <hannjimnz@gmail.com>
2026-05-27 19:43:38 -0600

    implementacion de sql al proyecto

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
+from database import engine, Base
+from models.producto import Producto
+
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def inicio():
